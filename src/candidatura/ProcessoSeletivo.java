e666b5b8baccd04b76fb170f280f8c1630268138
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-18 22:52:48 -0300

    implementada logica de selecao de candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,24 +1,48 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        // analisarCandidato(1500.0);
-        // analisarCandidato(2000.0);
-        analisarCandidato(2500.0);
+        selecionaCandidatos();
     }
 
-    static void analisarCandidato(double salarioPretendido){
+    static void selecionaCandidatos() {
+        String[] candidatos = { "GISELLE", "TIAGO", "MARCOS", "YAGO", "FELIPE", "MARIA", "TITO", "MIMI" };
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
         double salarioBase = 2000.0;
-        if(salarioBase > salarioPretendido) {
+
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato  " + candidato + " Solicitou este valor de salario: " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+
+            candidatosAtual++;
+        }
+    }
+
+    private static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
             System.out.println("Ligar para o candidato");
 
-        } else if ( salarioBase == salarioPretendido) {
+        } else if (salarioBase == salarioPretendido) {
             System.out.println("Ligar para o candidato com contra proposta");
 
         } else {
             System.out.println("Aguardando o resultado dos demais candidatos");
         }
-
 
     }
 }
